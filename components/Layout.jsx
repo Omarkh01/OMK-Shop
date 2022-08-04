45f56e1f85cba1fddf6739b995fac3374ef4e9c0
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-08-04 00:03:42 -0500

    adding new functions

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+
+import React from 'react';
 import Head from 'next/head';
 
-import Navbar from './NavBar';
+import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className='layout'>
+    <div className="layout">
       <Head>
-        <title>Omar's Store</title>
+        <title>JS Mastery Store</title>
       </Head>
       <header>
         <Navbar />
       </header>
-      <main className='main-container'>
+      <main className="main-container">
         {children}
       </main>
       <footer>
