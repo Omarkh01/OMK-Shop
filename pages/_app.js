d5f4aadfc6eda29c3973fd457f93c3a241ee041c
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-08-09 02:28:31 -0500

    sd

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { Layout } from '../components'
 import '../styles/globals.css'
 import { StateContext } from '../context/StateContext'
 
+// MyApp
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
