fabbc61611f6289d735a7531ec2346deccf7be20
Omar Khuatbyek <omar.khuatbek@gmail.com>
2022-08-09 02:09:26 -0500

    final commit

--- context/StateContext.js
+++ context/StateContext.js
@@ -90,7 +90,10 @@ export const StateContext = ({ children }) => {
         decQty,
         onAdd,
         toggleCartItemQuantity,
-        onRemove
+        onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities
       }}
     >
       {children}
